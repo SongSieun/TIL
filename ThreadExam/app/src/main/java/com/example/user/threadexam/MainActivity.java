327d034f82386a3c6e6ee5898100f68d386c8026
SieunSong <00sieun0608@naver.com>
2018-08-26 23:56:36 +0900

    Using AsyncTask

--- ThreadExam/app/src/main/java/com/example/user/threadexam/MainActivity.java
+++ ThreadExam/app/src/main/java/com/example/user/threadexam/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.user.threadexam;
 
+import android.os.AsyncTask;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextView;
     private ProgressBar mProgressBar;
-    private Handler mHandler = new Handler();
+    private DownLoadTask mDownLoadTask;
+    //private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +22,21 @@ public class MainActivity extends AppCompatActivity {
 
         mTextView = findViewById(R.id.textView);
         mProgressBar = findViewById(R.id.progressBar);
+
+        findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mDownLoadTask != null && !mDownLoadTask.isCancelled()){
+                    mDownLoadTask.cancel(true);
+                }
+            }
+        });
     }
 
     public void download(View view) {
-        new Thread(new Runnable() {
+        mDownLoadTask = new DownLoadTask();
+        mDownLoadTask.execute();
+/*        new Thread(new Runnable() {
             @Override
             public void run() {
                 // 오래 걸리는 일
@@ -43,6 +57,43 @@ public class MainActivity extends AppCompatActivity {
                     });
                 }
             }
-        }).start();
+        }).start();*/
+    }
+
+    class DownLoadTask extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            for (int i = 0; i <= 100; i++) {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                final int percent = i;
+                // UI 갱신 요청
+                publishProgress(percent);
+                // 취소 됨
+                if (isCancelled()) {
+                    break;
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            // UI 갱신 요청
+            mTextView.setText(values[0] + "%");
+            mProgressBar.setProgress(values[0]);
+        }
+
+        @Override
+        protected void onCancelled(Void aVoid) {
+            Toast.makeText(MainActivity.this, "취소 됨", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Toast.makeText(MainActivity.this, "완료 됨", Toast.LENGTH_SHORT).show();
+        }
     }
 }
