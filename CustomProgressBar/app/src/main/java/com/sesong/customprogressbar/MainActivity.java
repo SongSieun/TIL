e08253456eb41f022d37f9bfcc875204cf01fd99
SieunSong <00sieun0608@naver.com>
2018-09-12 23:57:37 +0900

    Custom Progressbar Exam

---
+++ CustomProgressBar/app/src/main/java/com/sesong/customprogressbar/MainActivity.java
@@ -0,0 +1,15 @@
+package com.sesong.customprogressbar;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
