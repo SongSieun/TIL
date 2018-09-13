b788e8805af6e3ed486ef7215cae9b83348443a2
SieunSong <00sieun0608@naver.com>
2018-09-13 23:54:58 +0900

    source code update

--- CustomProgressBar/app/src/main/java/com/sesong/customprogressbar/MainActivity.java
+++ CustomProgressBar/app/src/main/java/com/sesong/customprogressbar/MainActivity.java
@@ -1,15 +1,49 @@
 package com.sesong.customprogressbar;
 
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar progBar;
+    private TextView text;
+    private Handler mHandler = new Handler();
+    private int mProgressStatus = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progBar = (ProgressBar) findViewById(R.id.progressBar);
+        text = (TextView) findViewById(R.id.textView1);
+
+        dosomething();
+    }
+
+    public void dosomething() {
+        new Thread(new Runnable() {
+            public void run() {
+                final int presentage = 0;
+                while (mProgressStatus < 63) {
+                    mProgressStatus += 1;
+                    // Update the progress bar
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                            progBar.setProgress(mProgressStatus);
+                            text.setText("" + mProgressStatus + "%");
+                        }
+                    });
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
     }
 }
